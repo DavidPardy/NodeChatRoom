9582404cdb2b2c1960d8b17b1105385b68a8b1d2
davidpardy <dpardy87@gmail.com>
2014-08-21 22:51:32 -0700

    changed port number in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3700 || process.env.PORT;
+var port = Number(process.env.PORT || 5000);
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
